ea0355d28efb2c71c25fc7edac65b7afb32d4bd9
Divyanshu Singh <dsinecos@gmail.com>
2018-10-12 16:47:46 +0530

    setup dummy api to get drone's position
    the API exports two functions
    1. getPosition - Returns the current position when invoked
    2. watchPosition - Returns when the position of the drone changes

---
+++ tests/drone-simulator/positionAPI.js
@@ -0,0 +1,42 @@
+const faker = require('faker');
+
+function getPosition(lastPosition) {
+    if (!lastPosition) {
+        return Promise.resolve({
+            latitude: faker.address.latitude(),
+            longitude: faker.address.longitude(),
+            timestamp: (new Date()).getTime()
+        });
+    }
+    return Promise.resolve({
+        latitude: lastPosition.latitude + 0.00001,
+        longitude: lastPosition.longitude + 0.00001,
+        timestamp: (new Date()).getTime()
+    });
+}
+
+function watchPosition(fn) {
+    const randomInterval = ((Math.random() * 3) + 1) * 1000; // In seconds
+    let latitude = null;
+    let longitude = null;
+    let timestamp = null;
+
+    setTimeout(() => {
+        latitude = (latitude) ? Number(latitude) + 0.00001 : faker.address.latitude();
+        longitude = (longitude) ? Number(longitude) + 0.00001 : faker.address.longitude();
+        timestamp = (new Date()).getTime();
+
+        fn({
+            latitude,
+            longitude,
+            timestamp,
+        });
+
+        watchPosition(fn);
+    }, randomInterval);
+}
+
+module.exports = {
+    getPosition,
+    watchPosition
+};
