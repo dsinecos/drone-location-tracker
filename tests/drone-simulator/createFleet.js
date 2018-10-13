feb42ae17579467e4741b26f0bad2cdbbd1a0571
Divyanshu Singh <dsinecos@gmail.com>
2018-10-13 14:58:38 +0530

    provide udp port and host for location tracker instances

--- tests/drone-simulator/createFleet.js
+++ tests/drone-simulator/createFleet.js
@@ -1,5 +1,7 @@
 const FLEET_SIZE = process.env.FLEET_SIZE;
 const UPDATE_INTERVAL = process.env.UPDATE_INTERVAL || 1000;
+const UDP_PORT = process.env.UDP_PORT;
+const UDP_HOST = '127.0.0.1';
 
 const LocationTracker = require('../../lib/location-tracker/index');
 
@@ -7,7 +9,9 @@ for (let id = 1; id <= FLEET_SIZE; id++) {
     const DroneTracker = new LocationTracker({
         id,
         updateInterval: UPDATE_INTERVAL,
-        mode: 'VARIABLE'
+        mode: 'VARIABLE',
+        UDP_PORT,
+        UDP_HOST
     });
     
     DroneTracker.startTracking();
