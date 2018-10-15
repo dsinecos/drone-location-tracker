3fcf824ee254781a3c09eb23c952635e92f4a3d2
Divyanshu Singh <dsinecos@gmail.com>
2018-10-15 15:35:14 +0530

    update tests for ingestDataServer

--- tests/ingestDataServer.spec.js
+++ tests/ingestDataServer.spec.js
@@ -2,21 +2,22 @@ const chai = require('chai');
 const sinon = require('sinon');
 const UdpServer = require('../lib/backend/ingestDataServer');
 const PositionMemoryStore = require('../lib/backend/MemoryStore');
+const faker = require('faker');
 
 const expect = chai.expect;
 
 describe('IngestDataServer', function () {
-    beforeEach(function () {
-        this.storePostion = sinon.spy(PositionMemoryStore, 'setPositionFromDroneMessage');
-    });
-
-    afterEach(function () {
-        this.storePostion.restore();
-    });
-
     it('updates location data in PositionMemoryStore on receiving message', function () {
-        UdpServer.emit('message', 'location-data', { address: '127.0.0.1', port: '1234' });
-        expect(this.storePostion.calledOnce).to.be.true;
-        expect(this.storePostion.calledOnceWith('location-data')).to.be.true;
+        const droneId = `${1}`;
+        const latitude = `${faker.address.latitude()}`;
+        const longitude = `${faker.address.longitude()}`;
+        const timestamp = `${(new Date()).getTime()}`;
+
+        const droneMessage = `${droneId};${latitude},${longitude};${timestamp}`;
+
+        this.storePostion = sinon.spy(PositionMemoryStore, 'setPositionFromDroneMessage');
+
+        UdpServer.emit('message', droneMessage, { address: '127.0.0.1', port: '1234' });
+        expect(this.storePostion.calledOnceWith(droneMessage)).to.be.true;
     });
 });
